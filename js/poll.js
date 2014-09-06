4a862b6c515b44548cb6f3fc9f0636916c8e3b47
William Papper <contact@papper.me>
2014-09-06 17:12:10 -0500

    Moved buttons into a separate div

--- js/poll.js
+++ js/poll.js
@@ -115,8 +115,7 @@ function q1Yes() {
   document.location="http://teachoutwu.amicushq.com/landing/1";
 }
 function q1NotYet() {
-  document.getElementById("q1").style.display="none";
-  document.getElementById("ads").style.display="block";
+  document.getElementById("buttons").style.display="none";
   document.getElementById("q2").style.display="block";
   document.getElementById("inputaddress").focus();
 }
